095d4ae5ef1f0f5867fde4bd3e1b69250c7ae6d8
pdigvijay17 <pdigvijay17@gmail.com>
2017-07-11 23:30:40 +0530

    function created to get own info

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 #This is the access token for accessing the instagram API
-APP_ACCESS_TOKEN = '286308105.a90ec70.7691dca881004b60b57db1cc85505c1e'
+app_access_token = '286308105.a90ec70.7691dca881004b60b57db1cc85505c1e'
 
 #Token owner = shefaliyadv
 #Sandbox users = insta.bot.test.0, insta.bot.test.1, instabotmriutest0
 
 #This is the base URL for the instagram API.
-BASE_URL = 'https://api.instagram.com/v1/'
+base_url = 'https://api.instagram.com/v1/'
---
+++ my_info.py
@@ -0,0 +1,20 @@
+import requests
+from constants import base_url,app_access_token
+def my_info():
+  request_url = (base_url+ 'users/self/?access_token=%s') % (app_access_token)
+  print 'GET request url : %s' % (request_url)
+  user_info = requests.get(request_url).json()
+
+
+  if user_info['meta']['code'] == 200:
+
+      if len(user_info['data']):
+          print('Username: %s','blue') % (user_info['data']['username'])
+          print ('No. of followers: %s','blue') % (user_info['data']['counts']['followed_by'])
+          print ('No. of people you are following: %s','blue') % (user_info['data']['counts']['follows'])
+          print ('No. of posts: %s','blue') % (user_info['data']['counts']['media'])
+      else:
+          print ('User does not exist!','red')
+
+  else:
+      print ('Status code other than 200 received!','red')
