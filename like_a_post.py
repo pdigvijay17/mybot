029e07977ff31e8059cefb6cf01ca0a7e0caf4ca
pdigvijay17 <pdigvijay17@gmail.com>
2017-07-13 11:31:02 +0530

    function declared to post a like on the users media

---
+++ like_a_post.py
@@ -0,0 +1,17 @@
+import requests
+from constants import base_url,app_access_token
+from get_post_id import get_post_id
+#Working
+# 6. Function declarartion to post a like on the user's media.
+
+def like_a_post(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url = (base_url + 'media/%s/likes') % (media_id)
+    payload = {"access_token": app_access_token}
+    print 'POST request url : %s' % (request_url)
+    post_a_like = requests.post(request_url, payload).json()
+
+    if post_a_like['meta']['code'] == 200:
+        print ('Like was successful!','green')
+    else:
+        print ('Your like was unsuccessful. Please try again!','red')
